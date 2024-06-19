bffff48fc8f0276c1e411d88ecb1255b9e203e7b
Aleksandr Denisyuk <rb4@inbox.ru>
2024-06-19 16:07:08 +0300

    Update frequency random throttler

--- src/FrequencyRandomThrottler.php
+++ src/FrequencyRandomThrottler.php
@@ -6,6 +6,7 @@ namespace Orangesoft\Throttler;
 
 use Orangesoft\Throttler\Collection\CollectionInterface;
 use Orangesoft\Throttler\Collection\NodeInterface;
+use Orangesoft\Throttler\Collection\Sort\Desc;
 
 final class FrequencyRandomThrottler implements ThrottlerInterface
 {
@@ -24,12 +25,13 @@ final class FrequencyRandomThrottler implements ThrottlerInterface
             throw new \RuntimeException('Collection of nodes mustn\'t be empty.'); // @codeCoverageIgnore
         }
 
-        $total = \count($collection);
+        $sorted = $collection->sort(new Desc());
+        $total = \count($sorted);
         $lowerKey = (int) ceil($this->threshold * $total);
         $higherKey = $lowerKey + (1 < $total ? 1 : 0);
         $probability = mt_rand() / mt_getrandmax();
         $key = $this->frequency >= $probability ? mt_rand(1, $lowerKey) : mt_rand($higherKey, $total);
 
-        return $collection->get($key - 1);
+        return $sorted->get($key - 1);
     }
 }
